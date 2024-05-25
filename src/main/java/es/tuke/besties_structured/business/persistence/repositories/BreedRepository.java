6eecc62c617b76f530a9f88f3752df751dfe3dd4
CiscoTM <franciscotortillol@gmail.com>
2024-05-25 18:28:33 +0200

    beloved-pet :: entities

--- src/main/java/es/tuke/besties_structured/business/persistence/repositories/BreedRepository.java
+++
@@ -1,9 +0,0 @@
-package es.tuke.besties_structured.business.persistence.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import es.tuke.besties_structured.business.persistence.entities.BreedEntity;
-
-public interface BreedRepository extends JpaRepository<BreedEntity, Long> {
-    
-}
